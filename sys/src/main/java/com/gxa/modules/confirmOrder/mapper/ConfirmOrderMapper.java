279b0faf7c379ad1c311447ba80c1a4cc6a56f11
beam <1126719760@qq.com>
2022-11-12 18:53:53 +0800

    融合2.0

--- sys/src/main/java/com/gxa/modules/confirmOrder/mapper/ConfirmOrderMapper.java
+++ sys/src/main/java/com/gxa/modules/confirmOrder/mapper/ConfirmOrderMapper.java
@@ -3,6 +3,8 @@ package com.gxa.modules.confirmOrder.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gxa.modules.confirmOrder.dto.OrderDto;
 import com.gxa.modules.confirmOrder.entity.Order;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ConfirmOrderMapper extends BaseMapper<OrderDto> {
 }
